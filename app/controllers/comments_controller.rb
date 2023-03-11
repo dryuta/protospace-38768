23e2b91c6583da29930ce219601ac405cb1f6619
dryuta <tuyosanosaki@gmail.com>
2023-03-11 19:40:52 +0900

    tesuto

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,12 @@ class CommentsController < ApplicationController
       redirect_to  prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      #@comments  = @prototype.comment
+      @comments  = @prototype.comment
       render       "prototypes/show"
     end
   end
-
+  
+  private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
