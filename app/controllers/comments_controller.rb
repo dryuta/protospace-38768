f03fda2751761cdce8a9118726781c6a12bc5a43
dryuta <tuyosanosaki@gmail.com>
2023-03-11 14:55:53 +0900

    zz

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+  def create
+  comment = Comment.create(comment_params)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :prototype
+  has_many :comment
 
   validates :name,       presence: true
   validates :profile,    presence: true
