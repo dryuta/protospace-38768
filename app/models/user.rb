5f3c4c1503547f5908ba8ed1816e01ebc01521a9
dryuta <tuyosanosaki@gmail.com>
2023-03-17 14:32:28 +0900

    ユーザー詳細ページでユーザーの情報が表示されるようにしましょう

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :prototype
+  has_many :prototypes
   has_many :comments
 
   validates :name,       presence: true
