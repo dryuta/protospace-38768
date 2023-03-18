4414cac88932d08acff4312812098c2f5a02dde9
dryuta <tuyosanosaki@gmail.com>
2023-03-18 10:49:18 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -43,7 +43,7 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
-    render :index
+    redirect_to root_path
   end
 
   private
